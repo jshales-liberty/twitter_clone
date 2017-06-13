51fb99eab4b885af25ea5a017f9c8ba405a2e903
Yount <N0099324@lm.lmig.com>
2017-06-13 16:47:47 -0400

    HTML Pages created

--- src/main/java/twitter/Twitter.java
+++ src/main/java/twitter/Twitter.java
@@ -1,5 +1,43 @@
 package twitter;
 
-public class Twitter {
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.port;
 
+import org.jtwig.JtwigModel; 
+import org.jtwig.JtwigTemplate;
+
+public class Twitter {
+	public static void main(String[] args) {
+		
+		port(2613);
+		
+		get("/login", (request, response) -> {
+			return createLoginHTML();
+		});
+		
+		get("/createNewUser", (request, response) -> {
+			System.out.println("get practice");
+			return createNewUserHTML();
+		});
+		
+		post("/createNewUser", (request, response) -> {
+			return createLoginHTML();
+		});
+	}
+	
+	public static String createLoginHTML(){
+		JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/login.jTwig");
+        JtwigModel model = JtwigModel.newModel();
+
+        return template.render(model);
+	}
+	
+	public static String createNewUserHTML(){
+		JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/newUser.jTwig");
+		JtwigModel model = JtwigModel.newModel();
+
+		return template.render(model);
+	}
+	
 }
